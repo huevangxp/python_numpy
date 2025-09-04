e5145f905e58289804f6372daeed4dde57f8bddd
huevangxp <huevang933@gmail.com>
2025-09-04 10:33:53 +0700

    commit Thu 09/04/2025 10:33:53.11

--- panda.py
+++ panda.py
@@ -6,3 +6,12 @@ data = pd.read_excel('./file/user_account_payment.xlsx', sheet_name='Sheet1')
 print(data.columns)
 
 print(data['accNo'])
+
+print(data['accName'])
+
+money = data['amount']
+
+# sum money
+print(money.sum())
+
+
