9a77a750f5bb1a1efbf1990bcd338e73b178243d
huevangxp <huevang933@gmail.com>
2025-09-04 10:37:35 +0700

    commit Thu 09/04/2025 10:37:35.21

--- panda.py
+++ panda.py
@@ -15,8 +15,8 @@ money = data[['accName','amount']]
 
 print("sum money: ", "{:,.2f}".format(money['amount'].sum()))
 
-# show me top 5 money 
-print("top 5 money: ", money.nlargest(5, 'amount'))
+# show me top 5 money format currency
+print("top 5 money: ", money.nlargest(5, 'amount').style.format({'amount': '{:,.2f}'}))
 
 
 
