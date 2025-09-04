52cd56bbfdbe78d062cdbbf4df6a35f10257490f
huevangxp <huevang933@gmail.com>
2025-09-04 10:07:42 +0700

    commit Thu 09/04/2025 10:07:42.16

--- main.py
+++ main.py
@@ -9,4 +9,13 @@ print(array - 1)
 print(array * 2)
 print(array / 2)
 
+# Elementwise arithetic
+
+# Vectorized math funcs
+
+array = np.array([1.01, 2.5, 3.99])
+print(np.sqrt(array))
+print(np.exp(array))
+print(np.log(array))
+
 
