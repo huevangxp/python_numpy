a534b019a5ed8715dedf0baf219ea7594001293f
huevangxp <huevang933@gmail.com>
2025-09-04 10:33:11 +0700

    commit Thu 09/04/2025 10:33:11.10

--- panda.py
+++ panda.py
@@ -3,4 +3,6 @@ import pandas as pd
 # Create a DataFrame
 data = pd.read_excel('./file/user_account_payment.xlsx', sheet_name='Sheet1')
 
-print(data.columns(accNo))
+print(data.columns)
+
+print(data['accNo'])
