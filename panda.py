a839c84f4e65397a43e09c440d80770b820bb00f
huevangxp <huevang933@gmail.com>
2025-09-04 10:31:09 +0700

    commit Thu 09/04/2025 10:31:09.17

--- panda.py
+++ panda.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 # Create a DataFrame
-data = pd.read_excel('./file/user_account_payment.xlsx')
+data = pd.read_excel('./file/user_account_payment.xlsx', sheet_name='Sheet1')
 
-print(data.head())
+print(data.head(10))
