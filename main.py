3854d3962a8eeafce8b365509e1dca86a1b0ebbf
huevangxp <huevang933@gmail.com>
2025-09-04 09:18:07 +0700

    commit Thu 09/04/2025  9:18:07.15

--- main.py
+++ main.py
@@ -3,11 +3,4 @@ import numpy as np
 arr = np.array([1, 2, 3, 4, 5])
 print(arr)
 print(type(arr))
-
-arr = np.array([1, 2, 3, 4, 5], dtype=np.float64)
-print(arr)
-print(type(arr))
-
-arr = np.array([1, 2, 3, 4, 5], dtype=np.int32)
-print(arr)
-print(type(arr))
+ 
