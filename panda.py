6207137ddf01b8808f4d56e5e9037c92642485a2
huevangxp <huevang933@gmail.com>
2025-09-05 16:46:13 +0700

    new

--- panda.py
+++ panda.py
@@ -1,31 +1,17 @@
+ 
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Create a DataFrame
+# Cell 2 - Load Excel file
 data = pd.read_excel('./file/user_account_payment.xlsx', sheet_name='Sheet1')
+data.head()
 
+# Cell 3 - Data analysis
 print(data.columns)
-
 print(data['accNo'])
-
 print(data['accName'])
-
 money = data[['accName','amount']]
-
-# sum money add format currency
-
 print("sum money: ", "{:,.2f}".format(money['amount'].sum()))
-
-# show me top 5 money format currency
 print("top 5 money: ", money.nlargest(5, 'amount'))
-
-#show me to chart top 5 money and accName with matplotlib
-
 money.nlargest(5, 'amount').plot(kind='bar', x='accName', y='amount', color='blue')
 plt.show()
-
-
-
-
-
-
