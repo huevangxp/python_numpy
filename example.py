71df87b5a53f1c957c9dd76e4bd4fe56bff8b88c
huevangxp <huevang933@gmail.com>
2025-09-08 08:45:07 +0700

    commit Mon 09/08/2025  8:45:07.11

--- example.py
+++ example.py
@@ -1,22 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
-data = pd.read_excel('./file/user_account_payment.xlsx', sheet_name='Sheet1')
-data.head()
+df = pd.DataFrame({"A": [1, 2, 3], "B": [10, 20, 30]})
 
-df = pd.DataFrame(data)
-print(df)
-
-print(df.columns)
-print(df['accNo'])
-print(df['accName'])
-money = df[['accName','amount']]
-print("sum money: ", "{:,.2f}".format(money['amount'].sum()))
-print("top 5 money: ", money.nlargest(5, 'amount'))
-money.nlargest(5, 'amount').plot(kind='bar', x='accName', y='amount', color='blue')
-# check df money large 5 000 000
-money_large_5_000_000 = money[money['amount'] > 5000000]
-print("money large 5 000 000: ", money_large_5_000_000)
-money_large_5_000_000.to_csv("output.csv", index=False)
-plt.show()
-
+# Add a new column without modifying original
+df2 = df.assign(C=lambda x: x["A"] + x["B"])
+print(df2)
