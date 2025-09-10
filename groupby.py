e900b2baa21fd03b39d996bc8709896482ab857e
huevangxp <huevang933@gmail.com>
2025-09-10 16:31:31 +0700

    commit Wed 09/10/2025 16:31:31.20

--- groupby.py
+++ groupby.py
@@ -5,6 +5,6 @@ data = pd.read_excel("./file/user_account_payment.xlsx", sheet_name="Sheet1")
 print(data.head())
 
 # add bonus to money large than 5000000
-bonus = data.loc[data['amount'] > 5000000, 'amount'] = data['amount'] * 1.1
+bonus = data.loc[data['amount'] >= 5000000, 'amount'] = data['amount'] * 1.1
 
 print(data.head())
