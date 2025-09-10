cc8371887afffa9c97db92d46fa55b2998a57656
huevangxp <huevang933@gmail.com>
2025-09-10 10:32:59 +0700

    commit Wed 09/10/2025 10:32:59.14

--- groupby.py
+++ groupby.py
@@ -15,3 +15,7 @@ print(avg_salary)
 # Apply a custom function (e.g., max - min salary per department)
 # salary_range = df.groupby("Department")["Salary"].apply(lambda x: x.max() - x.min())
 # print(salary_range)
+
+# Group by multiple columns
+df_grouped = df.groupby(["Department", "Employee"])["Salary"].sum()
+print(df_grouped)
