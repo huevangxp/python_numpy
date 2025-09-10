991a91c5ba1a818a6169ae47a2290f3ee3d08bfa
huevangxp <huevang933@gmail.com>
2025-09-10 16:31:01 +0700

    commit Wed 09/10/2025 16:31:01.14

--- groupby.py
+++ groupby.py
@@ -1,67 +1,10 @@
 import pandas as pd
 
-data = {
-    "Department": ["IT", "HR", "IT", "Finance", "HR", "Finance"],
-    "Employee": ["Alice", "Bob", "Charlie", "David", "Eva", "Frank"],
-    "Salary": [5000, 4000, 5500, 6000, 4200, 6100]
-}
+data = pd.read_excel("./file/user_account_payment.xlsx", sheet_name="Sheet1")
 
-df = pd.DataFrame(data)
+print(data.head())
 
-# Average salary by department
-avg_salary = df.groupby("Department")["Salary"].mean()
-print(avg_salary)
+# add bonus to money large than 5000000
+bonus = data.loc[data['amount'] > 5000000, 'amount'] = data['amount'] * 1.1
 
-# Apply a custom function (e.g., max - min salary per department)
-# salary_range = df.groupby("Department")["Salary"].apply(lambda x: x.max() - x.min())
-# print(salary_range)
-
-# Group by multiple columns
-df_grouped = df.groupby(["Department", "Employee"])["Salary"].sum()
-print(df_grouped)
-
-# Group by with aggregation functions
-df_grouped = df.groupby("Department")["Salary"].agg([
-    ("mean_salary", "mean"),
-    ("max_salary", "max"),
-    ("min_salary", "min")
-])
-
-# Group by with pivot table
-df_pivot = df.pivot_table(values="Salary", index="Department", columns="Employee", aggfunc="sum")
-print(df_pivot)
-
-print(df_grouped)
-
-# Group by with pipe
-df_grouped = df.groupby("Department").pipe(lambda x: x["Salary"].mean())
-print(df_grouped)
-
-# SUM salary by department
-df_grouped = df.groupby("Department")["Salary"].sum()
-print(df_grouped)
-
-# SUM salary by employee
-df_grouped = df.groupby("Employee")["Salary"].sum()
-print(df_grouped)
-
-# SUM salary by department and employee
-df_grouped = df.groupby(["Department", "Employee"])["Salary"].sum()
-print(df_grouped)
-
-# add bonus to salary
-df_grouped = df.groupby("Department")["Salary"].apply(lambda x: x * 1.1)
-print(df_grouped)
-
-# add new column to df
-df["Bonus"] = df["Salary"].apply(lambda x: x * 0.1)
-print(df)
-
-# add bonus to salary
-df_grouped = df.groupby("Department")["Salary"].apply(lambda x: x * 1.1)
-print(df_grouped)
-
-# add bonus to salary
-df_grouped = df.groupby("Department")["Salary"].apply(lambda x: x * 1.1)
-print(df_grouped)
-
+print(data.head())
