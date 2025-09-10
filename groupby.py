3c31a0b26c55e75d6fa603774d4c72fad3cbd618
huevangxp <huevang933@gmail.com>
2025-09-10 16:34:14 +0700

    commit Wed 09/10/2025 16:34:14.19

--- groupby.py
+++ groupby.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 data = pd.read_excel("./file/user_account_payment.xlsx", sheet_name="Sheet1")
 
-print(data.head())
+# print(data.head())
 
 # find top 5 money
 print(data.nlargest(5, 'amount'))
