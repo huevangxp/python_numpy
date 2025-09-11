16e8de7ae9a39b416b314c97cebfb82adf2fa14b
huevangxp <huevang933@gmail.com>
2025-09-11 08:32:06 +0700

    commit Thu 09/11/2025  8:32:06.13

--- groupby.py
+++ groupby.py
@@ -13,7 +13,6 @@ userBottomMoney = data.nsmallest(5, 'amount')
 print(userBottomMoney)
  
 
-print(df.mean().nsmallest(5, 'amount'))
 
 # find top 5 money by accName and max
 print(df.max().nlargest(5, 'amount'))
